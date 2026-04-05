13b63c1452aac371e8166bded6c35c25c98b85af
lorsan <stasstrochewskij@gmail.com>
2026-04-05 10:57:50 +0300

    feat: yaml parser agent

--- internal/agent/utils/config_yaml/config.go
+++ internal/agent/utils/config_yaml/config.go
@@ -1,1 +1,31 @@
 package config_yaml
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type AgentConfig struct {
+	AppName    string `yaml:"app_name"`
+	HubConnect struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+	} `yaml:"hub"`
+}
+
+func NewConfig() (*AgentConfig, error) {
+	yamlFile, err := os.ReadFile("config.yaml")
+	if err != nil {
+		return nil, fmt.Errorf("failed open file: %v", err)
+	}
+
+	var cfg AgentConfig
+
+	if err = yaml.Unmarshal(yamlFile, &cfg); err != nil {
+		return nil, fmt.Errorf("failed read yaml: %v", err)
+	}
+
+	return &cfg, nil
+}
