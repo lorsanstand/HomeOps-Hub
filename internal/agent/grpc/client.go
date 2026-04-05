acbc100928c6082c28fe331bd1166b6c6eacd7a3
lorsan <stasstrochewskij@gmail.com>
2026-04-05 11:21:54 +0300

    refactor: connection agent -> hub

--- internal/agent/grpc/client.go
+++ internal/agent/grpc/client.go
@@ -1,1 +1,38 @@
 package grpc
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	pb "github.com/lorsanstand/HomeOps-Hub/api/gen/homeops"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type HomeOpsAgent struct {
+	conn pb.HubClient
+}
+
+func NewConnectAgent(address string) (*HomeOpsAgent, error) {
+	conn, err := grpc.NewClient(address)
+	if err != nil {
+		return nil, fmt.Errorf("failed connection hub: %v", err)
+	}
+
+	client := pb.NewHubClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	resp, err := client.Ping(ctx, &emptypb.Empty{})
+	if err != nil {
+		return nil, fmt.Errorf("failed connection hub: %v", err)
+	}
+
+	if resp.Pong != "Pong" {
+		return nil, fmt.Errorf("failed connection hub: %v", err)
+	}
+
+	return &HomeOpsAgent{conn: client}, nil
+}
