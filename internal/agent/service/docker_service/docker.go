4ce5f8206929ae6a61b41e461d9f2a412a1256aa
lorsan <stasstrcohewskij@gmial.com>
2026-04-11 18:43:56 +0300

    refactor: update logs

--- internal/agent/service/docker_service/docker.go
+++ internal/agent/service/docker_service/docker.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/moby/moby/api/types"
 	"github.com/moby/moby/api/types/container"
+	"github.com/rs/zerolog"
 )
 
 type dockerAPI interface {
@@ -14,18 +15,24 @@ type dockerAPI interface {
 
 type DockerService struct {
 	dockerClient dockerAPI
+	log          zerolog.Logger
 }
 
-func NewDockerService(api dockerAPI) *DockerService {
-	return &DockerService{dockerClient: api}
+func NewDockerService(api dockerAPI, logger zerolog.Logger) *DockerService {
+	return &DockerService{
+		dockerClient: api,
+		log:          logger.With().Str("component", "agent.serivce.docker").Logger(),
+	}
 }
 
 func (d *DockerService) CheckDockerDaemon(ctx context.Context) error {
 	_, err := d.dockerClient.Ping(ctx)
+	d.log.Debug().Msg("check docker")
 	return err
 }
 
 func (d *DockerService) ContainersList(ctx context.Context) ([]container.Summary, error) {
 	ContainersList, err := d.dockerClient.ContainerList(ctx, container.ListOptions{})
+	d.log.Debug().Msg("get container list")
 	return ContainersList, err
 }
